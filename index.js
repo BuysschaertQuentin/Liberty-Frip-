3bcd095bad214a288cb83153e4db66f862778f85
unknown <quentin.buysschaert@gmail.com>
2023-04-19 18:46:48 +0200

    add features

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const arrayPrice = [2, 3, 4, 5, 8, 10];
 
 let total = 0;
-
 let turnOver = [];
 
 function addPrice(id, price) {
@@ -14,7 +13,6 @@ function addPrice(id, price) {
     console.log(total);
   });
 }
-
 function addPriceToChoose() {
   const button = document.querySelector('.marques');
   button.addEventListener('click', () => {
@@ -29,10 +27,8 @@ function addPriceToChoose() {
     }
   });
 }
-
 function displayTotal() {
   const button = document.getElementById('8');
-
   button.addEventListener('click', () => {
     if (total === 0) {
       alert("Vous n'avez pas d'article");
@@ -43,15 +39,6 @@ function displayTotal() {
     }
   });
 }
-displayTotal();
-addPriceToChoose();
-addPrice(1, arrayPrice[0]);
-addPrice(2, arrayPrice[1]);
-addPrice(3, arrayPrice[2]);
-addPrice(4, arrayPrice[3]);
-addPrice(5, arrayPrice[4]);
-addPrice(6, arrayPrice[5]);
-
 const buttonOnPush = document.querySelectorAll('button');
 buttonOnPush.forEach(element => {
   element.addEventListener('click', e => {
@@ -63,5 +50,32 @@ buttonOnPush.forEach(element => {
   });
 });
 
-// - les boutons dépassent de leurs cercle (a cause de l'image)
+displayTotal();
+addPriceToChoose();
+addPrice(1, arrayPrice[0]);
+addPrice(2, arrayPrice[1]);
+addPrice(3, arrayPrice[2]);
+addPrice(4, arrayPrice[3]);
+addPrice(5, arrayPrice[4]);
+addPrice(6, arrayPrice[5]);
+
+// - les boutons dépassent de leurs cercle (a cause de l'image)  URGENT !!!!
 // - quand tu appuie sur un boutons cele retire l'image TOTAL
+
+// À prévoir absolument :
+
+// 2) afficher le nombre de clics déjà faits sur chaque bouton, en police 150 000, histoire que ça soit bien visible.
+
+// 3) prévoir un bouton "annuler" pour retirer des articles ou des clics excédentaires.
+
+// 4) le texte dans le bouton noir doit être en blanc pour une meilleure lisibilité
+
+// Pas essentiel / pas pour tout de suite
+
+// 1) implémenter un comptage des vêtements dans le détail de la journée
+
+// 2) ajouter une fonction prix cassés (-30% sur le total par exemple) GALERE
+
+// 3) basculer sur une version tactile pour iPad.
+
+// 4) devenir richissime et acheter une île dans le Pacifique pour y élever des dinosaures.
